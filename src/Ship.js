89b691360a7197ff0d223e4951cf1839b17e42cf
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2025-10-01 15:54:50 -0400

    feat(Ship): implement Ship(shipType) to satisfy test setting shipType property (deprecated — kept for reference)

---
+++ src/Ship.js
@@ -0,0 +1,13 @@
+// Aircraft Carrier: The largest ship, taking up five spaces on the board.
+// Battleship: The second-largest ship, covering four spaces.
+// Cruiser: A three-space ship.
+// Submarine: Another three-space ship.
+// Destroyer: The smallest ship in the game, occupying two spaces.
+
+class Ship {
+  constructor(shipType) {
+    this.shipType = shipType;
+  }
+}
+
+export default Ship;
