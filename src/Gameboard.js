34b4a3fa8dc5fefcbb3357e0deb65e7071c6e09c
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2025-10-07 21:38:37 -0400

    feat(Gameboard.placeShip): adds shipLength number of entries to activeShipCells Map with cells as keys and the ship instance as values, starting from the passed coordinate and extending based on orientation

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,12 +1,42 @@
+import Ship from "./Ship.js";
+
 class Gameboard {
   constructor() {
-    this.activeShipFleet = [
-      "aircraftCarrier",
-      "battleship",
-      "cruiser",
-      "submarine",
-      "destroyer",
-    ];
+    this.activeShipFleet = ["aircraftCarrier", "battleship", "cruiser", "submarine", "destroyer"];
+    this.activeShipCells = new Map();
+  }
+  placeShip(shipName, shipPlacementCell, orientation) {
+    const shipToPlace = new Ship(shipName);
+    const shipLength = shipToPlace.shipLength;
+    let currentCell = shipPlacementCell;
+
+    for (let i = 0; i < shipLength; i++) {
+      this.activeShipCells.set(currentCell, shipToPlace);
+      currentCell = getNextCell(currentCell, orientation);
+    }
+    function getNextCell(currentCell, orientation) {
+      const gameboardColumns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+      const gameboardRows = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+      if (orientation === "horizontal") {
+        // Get current column as string and make uppercase
+        const currentCellColumn = currentCell.split("")[0].toUpperCase();
+        // Get index of gameboardColumns item that matches currentCellColumn
+        const letterIndex = gameboardColumns.indexOf(currentCellColumn);
+        // Return a string (joined array) that its former array consisted of the incremented column and same row
+        let nextCoordinate = [gameboardColumns[letterIndex + 1], currentCell.split("")[1]];
+        nextCoordinate = nextCoordinate.join("");
+        return nextCoordinate;
+      } else {
+        // Get current row as string
+        const currentCellRow = currentCell.split("")[1];
+        // Get index of gameboardRows item that matches currentCellRow
+        const letterIndex = gameboardRows.indexOf(currentCellRow);
+        // Return a string (joined array) that its former array consisted of the same column and incremented row
+        let nextCoordinate = [currentCell.split("")[0], gameboardRows[letterIndex + 1]];
+        nextCoordinate = nextCoordinate.join("");
+        return nextCoordinate;
+      }
+    }
   }
 }
 
--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -1,13 +1,8 @@
 import Gameboard from "../Gameboard.js";
+import Ship from "../Ship.js";
 
 describe("Gameboard Class", () => {
-  it.each([
-    ["aircraftCarrier"],
-    ["battleship"],
-    ["cruiser"],
-    ["submarine"],
-    ["destroyer"],
-  ])(
+  it.each([["aircraftCarrier"], ["battleship"], ["cruiser"], ["submarine"], ["destroyer"]])(
     "activeShipFleet initializes with an array with string values; aircraftCarrier, battleship, cruiser, submarine, destroyer",
     (testShip) => {
       const testGameboard = new Gameboard();
@@ -16,6 +11,48 @@ describe("Gameboard Class", () => {
   );
 });
 
+describe("placeShip method", () => {
+  jest.mock("../Ship", () => {
+    return jest.fn(() => {
+      return {
+        shipName: "aircraftCarrier",
+        shipLength: 5,
+      };
+    });
+  });
+
+  describe("placeShip horizontal", () => {
+    const testGameboard = new Gameboard();
+    const testShip = new Ship("aircraftCarrier");
+
+    testGameboard.placeShip("aircraftCarrier", "A1", "horizontal");
+    it.each([["A1"], ["B1"], ["C1"], ["D1"], ["E1"]])(
+      "adds shipLength number of entries to activeShipCells Map with cells as keys and the ship instance as values, starting from the passed coordinate and extending based on horizontal orientation",
+      (expectationCell) => {
+        expect(testGameboard.activeShipCells.has(expectationCell)).toBe(true);
+        expect(testGameboard.activeShipCells.get(expectationCell)).toEqual(testShip);
+      },
+    );
+  });
+
+  describe("placeShip vertical", () => {
+    const testGameboard = new Gameboard();
+    const testShip = new Ship("submarine");
+
+    testGameboard.placeShip("submarine", "C2", "vertical");
+    it.each([["C2"], ["C3"], ["C4"]])(
+      "adds shipLength number of entries to activeShipCells Map with cells as keys and the ship instance as values, starting from the passed coordinate and extending based on vertical orientation",
+      (expectationCell) => {
+        expect(testGameboard.activeShipCells.has(expectationCell)).toBe(true);
+        expect(testGameboard.activeShipCells.get(expectationCell)).toEqual(testShip);
+      },
+    );
+  });
+
+  // Arguments: shipName, shipPlacementCell, orientation
+  // Create new variable ship for new instance of Ship's (new Ship(shipName)), and variable shipLength for ship length (ship.length)
+  // Dependent on shipLength, passed shipPlacementCell, orientation the following actions iterate the same amount of times as shipLength: iteravley increments cell entries either "column letter" or "row number" based on orientation, and entry to activeCells with cell entry being key and ship being value
+});
 //
 
 // Gameboards should be able to place ships at specific coordinates by calling the ship factory or class.
@@ -27,3 +64,8 @@ describe("Gameboard Class", () => {
 //Game board has array/map or active  cells that show how many cells each ship takes up, and ship instance . Object. Key A1 Object
 
 // git add Gameboard.test.js
+
+/*
+  
+re amend commit
+  */
