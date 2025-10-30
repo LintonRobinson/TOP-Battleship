7fbc39b2690e332f977981a54b4805d3eb974cc6
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2025-10-30 13:49:19 -0400

    feat(ui): (when the page is clicked) If the event targets id is "startGame": - fades out start game screen - changes div with id  "start-game-wrapper" wrapper to none - displays select game mode screen

--- src/game.js
+++ src/game.js
@@ -1,8 +1,28 @@
 function startGame() {
+  const startGameWrapper = document.querySelector("#start-game-wrapper");
+  const selectGameModeWrapper = document.querySelector("#select-mode-wrapper");
+  const playerVsPlayerBtn = document.querySelector("#player-vs-player");
+
   document.addEventListener("click", (event) => {
+    // Start game button click displays select game mode screen
     if (event.target.id === "startGame") {
-      document.querySelector("#start-game-wrapper").classList.add("fadeOut");
-      document.querySelector("#select-mode-wrapper").classList.add("fadeIn");
+      startGameWrapper.classList.add("fadeOut");
+      addFadeAnimationDelay(() => (startGameWrapper.style.display = "none"));
+      selectGameModeWrapper.classList.add("fadeIn");
+
+      selectGameModeWrapper.classList.add("active-screen");
+      addFadeAnimationDelay(() => {
+        selectGameModeWrapper.classList.remove("fadeIn");
+        selectGameModeWrapper.classList.remove("off-screen-start-position");
+      });
+    }
+
+    // Start game button click displays select game mode screen
+    if (event.target.id === "player-vs-player") {
+      selectGameModeWrapper.classList.remove("fadeIn");
+      selectGameModeWrapper.classList.add("fadeOut");
+      //addFadeAnimationDelay(() => (startGameWrapper.style.display = "none"));
+      //playerVsPlayerBtn.classList.add("fadeIn");
     }
   });
 
