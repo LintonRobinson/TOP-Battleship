731cd7e15545d10a195c68deecf5ea69d3d2ce79
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2025-10-30 12:09:22 -0400

    chore: wip - save work

--- src/game.js
+++ src/game.js
@@ -1,11 +1,14 @@
-const startGameBtn = document.querySelector("startGame");
+function startGame() {
+  document.addEventListener("click", (event) => {
+    if (event.target.id === "startGame") {
+      document.querySelector("#start-game-wrapper").classList.add("fadeOut");
+      document.querySelector("#select-mode-wrapper").classList.add("fadeIn");
+    }
+  });
 
-document.addEventListener("click", (event) => {
-  if (event.target.id === "startGame") {
-    alert("Ya Mama");
+  function addFadeAnimationDelay(callback) {
+    setTimeout(callback, 1000);
   }
-});
-
-function addFadeAnimationDelay(callback) {
-  setTimeout(callback, 1000);
 }
+
+export default startGame;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import "./style.css";
+import startGame from "./game";
 
-document.addEventListener("click", (event) => {
-  if (event.target.id === "startGame") {
-    alert("Ya Mama");
-  }
-});
+document.addEventListener("DOMContentLoaded", startGame);
