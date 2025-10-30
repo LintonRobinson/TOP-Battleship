1348f19b2177356e46b1840bf30d59710288fb3b
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2025-10-30 17:05:33 -0400

    chore: wip - save work

--- src/game.js
+++ src/game.js
@@ -1,16 +1,18 @@
 function startGame() {
   const startGameWrapper = document.querySelector("#start-game-wrapper");
   const selectGameModeWrapper = document.querySelector("#select-mode-wrapper");
-  const playerVsPlayerBtn = document.querySelector("#player-vs-player");
+  const enterPlayerNamesWrapper = document.querySelector("#enter-player-names-wrapper");
 
   document.addEventListener("click", (event) => {
-    // Start game button click displays select game mode screen
+    // Start game button click  hides start game screen and displays select game mode screen
     if (event.target.id === "startGame") {
+      // Hide start game screen
       startGameWrapper.classList.add("fadeOut");
       addFadeAnimationDelay(() => (startGameWrapper.style.display = "none"));
+      // Show select game mode screen
       selectGameModeWrapper.classList.add("fadeIn");
-
       selectGameModeWrapper.classList.add("active-screen");
+      // remove fadeIn/off-screen-start-position class from selectGameModeWrapper (sekect game mode screen)
       addFadeAnimationDelay(() => {
         selectGameModeWrapper.classList.remove("fadeIn");
         selectGameModeWrapper.classList.remove("off-screen-start-position");
@@ -19,8 +21,11 @@ function startGame() {
 
     // Start game button click displays select game mode screen
     if (event.target.id === "player-vs-player") {
-      selectGameModeWrapper.classList.remove("fadeIn");
       selectGameModeWrapper.classList.add("fadeOut");
+      addFadeAnimationDelay(() => (selectGameModeWrapper.style.display = "none"));
+
+      enterPlayerNamesWrapper.classList.add("fadeIn");
+      enterPlayerNamesWrapper.classList.add("active-screen");
       //addFadeAnimationDelay(() => (startGameWrapper.style.display = "none"));
       //playerVsPlayerBtn.classList.add("fadeIn");
     }
